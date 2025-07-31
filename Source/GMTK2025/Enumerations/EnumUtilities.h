55739c50335817e99e0c27510877ca36a467f33a
itaruf <79866301+itaruf@users.noreply.github.com>
2025-07-31 18:42:09 +0200

    Add an Enum Utilities with random enum value getter

---
+++ Source/GMTK2025/Enumerations/EnumUtilities.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "UObject/UnrealType.h"
+#include "Math/UnrealMathUtility.h"
+
+template<typename TEnum>
+static TEnum GetRandomEnumValue()
+{
+	const UEnum* EnumPtr = StaticEnum<TEnum>();
+	check(EnumPtr);
+	
+	int32 Num = EnumPtr->NumEnums();
+	
+	int32 RandIndex = FMath::RandRange(0, Num - 1);
+	return static_cast<TEnum>(EnumPtr->GetValueByIndex(RandIndex));
+}
