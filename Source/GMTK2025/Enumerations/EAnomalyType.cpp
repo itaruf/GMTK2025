be5f6654e325c09d222add5e4a2e4c1c51103d6c
itaruf <79866301+itaruf@users.noreply.github.com>
2025-07-31 17:46:19 +0200

    Add an Anomaly Type enumeration to define possible anomalies

---
+++ Source/GMTK2025/Enumerations/EAnomalyType.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Enumerations/EAnomalyType.h"
+
+EAnomalyType::EAnomalyType()
+{
+}
+
+EAnomalyType::~EAnomalyType()
+{
+}
---
+++ Source/GMTK2025/Enumerations/EAnomalyType.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class GMTK2025_API EAnomalyType
+{
+public:
+	EAnomalyType();
+	~EAnomalyType();
+};
