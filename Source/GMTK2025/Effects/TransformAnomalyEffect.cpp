3f390023d1f6bbb1f6c46cdae50add7853bce02c
itaruf <79866301+itaruf@users.noreply.github.com>
2025-08-01 20:38:03 +0200

    Create an Anomaly Point blueprint

---
+++ Source/GMTK2025/Effects/TransformAnomalyEffect.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TransformAnomalyEffect.h"
+#include "GameFramework/Actor.h"
+#include "Math/UnrealMathUtility.h"
+
+void UTransformAnomalyEffect::Apply_Implementation(AActor* Target)
+{
+	if (!Target) return;
+
+	// Look for the config component on this actor:
+	if (UTransformAnomalyConfigComponent* Config =
+		Target->FindComponentByClass<UTransformAnomalyConfigComponent>())
+	{
+		if (Config->PossibleTargetPoints.Num() > 0)
+		{
+			int32 Idx = FMath::RandRange(0, Config->PossibleTargetPoints.Num() - 1);
+			if (ATargetPoint* Chosen = Config->PossibleTargetPoints[Idx])
+			{
+				Target->SetActorLocationAndRotation(
+					Chosen->GetActorLocation(),
+					Chosen->GetActorRotation()
+				);
+				return;
+			}
+		}
+	}
+
+	UE_LOG(LogTemp, Warning,
+		   TEXT("TransformAnomalyEffect on %s: missing TransformAnomalyConfigComponent or no points !"),
+		   *Target->GetName());
+}
---
+++ Source/GMTK2025/Effects/TransformAnomalyEffect.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AnomalyEffect.h"
+#include "Components/TransformAnomalyConfigComponent.h"
+#include "TransformAnomalyEffect.generated.h"
+
+UCLASS(Blueprintable)
+class GMTK2025_API UTransformAnomalyEffect : public UAnomalyEffect
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Apply_Implementation(AActor* Target) override;
+};
