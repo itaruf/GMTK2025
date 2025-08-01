80cc75d9fafe0fe6b3588c61a7204d25d6873fe6
itaruf <79866301+itaruf@users.noreply.github.com>
2025-08-01 13:56:30 +0200

    Add an Anomaly Subsystem to handle retrieval / activation of Anomalies

---
+++ Source/GMTK2025/Subsystems/AnomalySubsystem.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnomalySubsystem.h"
+
+void UAnomalySubsystem::Initialize(FSubsystemCollectionBase& SubsystemCollectionBase)
+{
+	Super::Initialize(SubsystemCollectionBase);
+}
+
+void UAnomalySubsystem::Deinitialize()
+{
+	Super::Deinitialize();
+}
+
+void UAnomalySubsystem::RegisterComponent(UAnomalyComponent* Comp)
+{
+	Components.Add(Comp);
+}
+
+void UAnomalySubsystem::UnregisterComponent(UAnomalyComponent* Comp)
+{
+	Components.Remove(Comp);
+}
+
+void UAnomalySubsystem::TriggerAnomaly()
+{
+	if (Components.Num() == 0) return;
+
+	int32 Idx = FMath::RandRange(0, Components.Num() - 1);
+	UAnomalyComponent* Chosen = Components.Array()[Idx];
+
+	Chosen->Select();
+	Chosen->Apply();
+}
---
+++ Source/GMTK2025/Subsystems/AnomalySubsystem.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "Components/AnomalyComponent.h"
+#include "AnomalySubsystem.generated.h"
+
+UCLASS()
+class GMTK2025_API UAnomalySubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Initialize(FSubsystemCollectionBase&) override;
+	virtual void Deinitialize() override;
+
+	void RegisterComponent(UAnomalyComponent* Comp);
+	void UnregisterComponent(UAnomalyComponent* Comp);
+
+public:
+	void TriggerAnomaly();
+
+private:
+	UPROPERTY()
+	TSet<UAnomalyComponent*> Components;
+	FTimerHandle AnomalyTimer;
+};
