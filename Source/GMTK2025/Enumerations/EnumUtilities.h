075b2c3b11c238eaf4325a20c1f25116769e9ea7
itaruf <79866301+itaruf@users.noreply.github.com>
2025-08-01 14:13:48 +0200

    Modify Random Enum Value getters to include starting index

--- Source/GMTK2025/Enumerations/EnumUtilities.h
+++ Source/GMTK2025/Enumerations/EnumUtilities.h
@@ -3,14 +3,21 @@
 #include "UObject/UnrealType.h"
 #include "Math/UnrealMathUtility.h"
 
-template<typename TEnum>
-static TEnum GetRandomEnumValue()
+template <typename TEnum>
+TEnum GetRandomEnumValue(int32 StartIndex = 0)
 {
 	const UEnum* EnumPtr = StaticEnum<TEnum>();
 	check(EnumPtr);
-	
+
 	int32 Num = EnumPtr->NumEnums();
-	
-	int32 RandIndex = FMath::RandRange(0, Num - 1);
+	if (Num == 0)
+	{
+		return TEnum(0);
+	}
+
+	int32 ClampedStart = FMath::Clamp(StartIndex, 0, Num - 1);
+
+	int32 RandIndex = FMath::RandRange(ClampedStart, Num - 1);
+
 	return static_cast<TEnum>(EnumPtr->GetValueByIndex(RandIndex));
 }
