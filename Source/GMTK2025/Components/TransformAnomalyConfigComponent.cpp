5e5d1b9798bf2a550516466e669fb1478fe73093
itaruf <79866301+itaruf@users.noreply.github.com>
2025-08-01 20:39:03 +0200

    Add a Transform Anomaly Config Component for mesh previsualization of position/rotation

---
+++ Source/GMTK2025/Components/TransformAnomalyConfigComponent.cpp
@@ -0,0 +1,145 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TransformAnomalyConfigComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/Actor.h"
+#include "Engine/StaticMesh.h"
+#include "Materials/MaterialInterface.h"
+#include "UObject/ConstructorHelpers.h"
+
+UTransformAnomalyConfigComponent::UTransformAnomalyConfigComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	bAutoActivate = true;
+
+#if WITH_EDITOR
+	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bStartWithTickEnabled = true;
+	bTickInEditor = true;
+#endif
+}
+
+void UTransformAnomalyConfigComponent::BeginPlay()
+{
+	Super::BeginPlay();
+#if WITH_EDITOR
+	ClearPreviews();
+#endif
+}
+
+#if WITH_EDITOR
+
+void UTransformAnomalyConfigComponent::TickComponent(float DeltaTime, enum ELevelTick TickType,
+                                                     FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (!bShowPreviews)
+		return;
+
+	bool bAnyMoved = false;
+	for (ATargetPoint* TP : PossibleTargetPoints)
+	{
+		if (!TP) continue;
+		FTransform Current = TP->GetActorTransform();
+		if (!LastTargetPointTransforms.Contains(TP) || !Current.Equals(LastTargetPointTransforms[TP]))
+		{
+			bAnyMoved = true;
+			LastTargetPointTransforms.FindOrAdd(TP) = Current;
+		}
+	}
+	if (bAnyMoved)
+	{
+		RebuildPreviews();
+	}
+}
+
+void UTransformAnomalyConfigComponent::OnRegister()
+{
+	Super::OnRegister();
+	RebuildPreviews();
+}
+
+void UTransformAnomalyConfigComponent::OnUnregister()
+{
+	ClearPreviews();
+	Super::OnUnregister();
+}
+
+void UTransformAnomalyConfigComponent::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+	RebuildPreviews();
+}
+
+void UTransformAnomalyConfigComponent::PostEditComponentMove(bool bFinished)
+{
+	if (bFinished)
+	{
+		RebuildPreviews();
+	}
+}
+
+void UTransformAnomalyConfigComponent::ClearPreviews()
+{
+	for (UStaticMeshComponent* Mesh : PreviewMeshes)
+	{
+		if (Mesh)
+		{
+			Mesh->DestroyComponent();
+		}
+	}
+	PreviewMeshes.Empty();
+}
+
+void UTransformAnomalyConfigComponent::RebuildPreviews()
+{
+#if WITH_EDITOR
+	if (IsRunningGame() || GIsPlayInEditorWorld) return;
+#endif
+
+	ClearPreviews();
+
+	if (!bShowPreviews || !GetOwner())
+		return;
+
+	UStaticMeshComponent* OwnerMesh = GetOwner()->FindComponentByClass<UStaticMeshComponent>();
+	if (!OwnerMesh || !OwnerMesh->GetStaticMesh())
+		return;
+
+	UMaterialInterface* GhostMat = Cast<UMaterialInterface>(
+		StaticLoadObject(UMaterialInterface::StaticClass(), nullptr,
+		                 TEXT(
+			                 "/Engine/EngineDebugMaterials/VertexColorViewMode_GreenOnly.VertexColorViewMode_GreenOnly")));
+
+	int32 Counter = 0;
+	for (ATargetPoint* TP : PossibleTargetPoints)
+	{
+		if (!TP)
+			continue;
+
+		FName PreviewName = MakeUniqueObjectName(this, UStaticMeshComponent::StaticClass(),
+		                                         FName(*FString::Printf(
+			                                         TEXT("AnomalyPreviewMesh_%p_%d"), this, Counter++)));
+		UStaticMeshComponent* Mesh = NewObject<UStaticMeshComponent>(this, PreviewName);
+
+		Mesh->SetStaticMesh(OwnerMesh->GetStaticMesh());
+
+		FTransform PreviewTransform = OwnerMesh->GetRelativeTransform() * TP->GetActorTransform();
+		Mesh->SetWorldTransform(PreviewTransform);
+
+		Mesh->SetMobility(EComponentMobility::Movable);
+		Mesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+		Mesh->SetVisibility(true);
+		Mesh->bIsEditorOnly = true;
+		if (GhostMat)
+		{
+			Mesh->SetMaterial(0, GhostMat);
+		}
+		Mesh->RegisterComponentWithWorld(GetWorld());
+
+		PreviewMeshes.Add(Mesh);
+	}
+}
+
+#endif
---
+++ Source/GMTK2025/Components/TransformAnomalyConfigComponent.h
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Engine/TargetPoint.h"
+#include "TransformAnomalyConfigComponent.generated.h"
+
+class UStaticMeshComponent;
+
+/**
+ * Drop this on any Actor to configure world-space points for Transform anomalies
+ * and visualize them as ghost meshes at each target point.
+ */
+UCLASS(ClassGroup=(Anomaly), meta=(BlueprintSpawnableComponent))
+class GMTK2025_API UTransformAnomalyConfigComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UTransformAnomalyConfigComponent();
+
+	/** World-space TargetPoints whose rotations/positions will be used */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Transform Anomaly")
+	TArray<ATargetPoint*> PossibleTargetPoints;
+
+	/** Toggle to show or hide the in-editor preview ghost meshes */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Transform Anomaly")
+	bool bShowPreviews = true;
+
+#if WITH_EDITOR
+	virtual void OnRegister() override;
+	virtual void OnUnregister() override;
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+	virtual void PostEditComponentMove(bool bFinished);
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	TMap<ATargetPoint*, FTransform> LastTargetPointTransforms;
+
+#endif
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+#if WITH_EDITORONLY_DATA
+	/** Currently spawned preview mesh components */
+	UPROPERTY(Transient)
+	TArray<UStaticMeshComponent*> PreviewMeshes;
+
+	/** Destroy all existing preview mesh components */
+	void ClearPreviews();
+	/** Rebuild ghost mesh previews at each TargetPoint */
+	void RebuildPreviews();
+#endif
+};
