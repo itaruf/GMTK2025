1cdd7679258a40c5408ef137032d85caacbe0586
itaruf <79866301+itaruf@users.noreply.github.com>
2025-08-01 13:53:57 +0200

    Add a Position Anomaly Effect that use Target Points to be applied

---
+++ Source/GMTK2025/Effects/PositionAnomalyEffect.cpp
@@ -0,0 +1,29 @@
+﻿// PositionAnomalyEffect.cpp
+#include "PositionAnomalyEffect.h"
+#include "GameFramework/Actor.h"
+#include "Math/UnrealMathUtility.h"
+
+void UPositionAnomalyEffect::Apply_Implementation(AActor* Target)
+{
+	if (!Target) return;
+
+	// Look for the config component on this actor:
+	if (UPositionAnomalyConfigComponent* Config =
+		Target->FindComponentByClass<UPositionAnomalyConfigComponent>())
+	{
+		// 1) If TargetPoints are filled, use those:
+		if (Config->TargetPoints.Num() > 0)
+		{
+			int32 Idx = FMath::RandRange(0, Config->TargetPoints.Num() - 1);
+			if (ATargetPoint* Chosen = Config->TargetPoints[Idx])
+			{
+				Target->SetActorLocation(Chosen->GetActorLocation());
+				return;
+			}
+		}
+	}
+
+	UE_LOG(LogTemp, Warning,
+	       TEXT("PositionAnomalyEffect on %s: missing PositionAnomalyConfigComponent or no points !"),
+	       *Target->GetName());
+}
---
+++ Source/GMTK2025/Effects/PositionAnomalyEffect.h
@@ -0,0 +1,17 @@
+﻿// TransformAnomalyEffect.h
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AnomalyEffect.h"
+#include "Components/PositionAnomalyConfigComponent.h"
+#include "PositionAnomalyEffect.generated.h"
+
+UCLASS(Blueprintable)
+class GMTK2025_API UPositionAnomalyEffect : public UAnomalyEffect
+{
+	GENERATED_BODY()
+
+public:
+	/** Runtime: grabs its UTransformAnomalyConfigComponent on the target actor */
+	virtual void Apply_Implementation(AActor* Target) override;
+};
