e54327b22ea66682aba252efeca8ab32311ba666
itaruf <79866301+itaruf@users.noreply.github.com>
2025-08-02 14:53:18 +0200

    Add a Loop Subsystem to manage the game loop (e.g., win/fail conditions)

---
+++ Source/GMTK2025/Subsystems/LoopSubsystem.cpp
@@ -0,0 +1,96 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LoopSubsystem.h"
+#include "AnomalySubsystem.h"
+#include "Kismet/GameplayStatics.h"
+
+void ULoopSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+	StartLoop();
+}
+
+void ULoopSubsystem::StartLoop()
+{
+	// Choose total iterations TODO: remove hard-coded values
+	TotalIterations = FMath::RandRange(5, 7);
+
+	// 1 in (TotalIterations+1) chance to have no anomaly
+	int32 Choice = FMath::RandRange(0, TotalIterations);
+	AnomalyIteration = (Choice == 0 ? 0 : Choice);
+
+	CurrentIteration = 0;
+}
+
+void ULoopSubsystem::ResetLoop()
+{
+	// Anomaly entity needs to revert to its initial state
+	if (auto* Anom = GetWorld()->GetSubsystem<UAnomalySubsystem>())
+	{
+		Anom->Reset();
+	}
+}
+
+void ULoopSubsystem::ContinueLoop()
+{
+	CurrentIteration++;
+
+	// If they escaped past the anomaly without reporting it
+	if (CurrentIteration - 1 == AnomalyIteration && AnomalyIteration != 0)
+	{
+		// They didn’t call ReportAnomaly(), so they missed it
+		FailLoop();
+		return;
+	}
+
+	// Trigger anomaly only on the chosen iteration
+	if (CurrentIteration == AnomalyIteration && AnomalyIteration != 0)
+	{
+		if (auto* Anom = GetWorld()->GetSubsystem<UAnomalySubsystem>())
+		{
+			Anom->TriggerAnomaly();
+		}
+	}
+
+	// If this was the last iteration
+	if (CurrentIteration >= TotalIterations)
+	{
+		// If no anomaly was ever scheduled:
+		if (AnomalyIteration == 0)
+		{
+			SucceedLoop();
+		}
+		else
+		{
+			// They made it to the end without reporting the scheduled anomaly
+			FailLoop();
+		}
+	}
+}
+
+void ULoopSubsystem::ReportAnomaly()
+{
+	// Called when player “spots” something
+	if (CurrentIteration == AnomalyIteration && AnomalyIteration != 0)
+	{
+		SucceedLoop();
+	}
+	else
+	{
+		FailLoop();
+	}
+}
+
+void ULoopSubsystem::FailLoop()
+{
+	OnLoopEnd.Broadcast(false);
+	// Reset the loop
+	ResetLoop();
+	// Restart the loop
+	StartLoop();
+}
+
+void ULoopSubsystem::SucceedLoop()
+{
+	OnLoopEnd.Broadcast(true);
+}
---
+++ Source/GMTK2025/Subsystems/LoopSubsystem.h
@@ -0,0 +1,66 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "LoopSubsystem.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnLoopEvent, bool, bSuccess);
+
+UCLASS()
+class GMTK2025_API ULoopSubsystem : public UWorldSubsystem
+{
+private:
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	int32 GetTotalIterations() const
+	{
+		return TotalIterations;
+	}
+
+	UFUNCTION(BlueprintCallable)
+	int32 GetAnomalyIteration() const
+	{
+		return AnomalyIteration;
+	}
+
+	UFUNCTION(BlueprintCallable)
+	int32 GetCurrentIteration() const
+	{
+		return CurrentIteration;
+	}
+
+	// Called once at game start or after a loss
+	UFUNCTION(BlueprintCallable)
+	void StartLoop();
+
+	// Called once at game start or after a loss
+	UFUNCTION(BlueprintCallable)
+	void ResetLoop();
+
+	// Call when player reports “I see an anomaly”
+	UFUNCTION(BlueprintCallable)
+	void ReportAnomaly();
+
+	// Call when player steps through the mirror to continue
+	UFUNCTION(BlueprintCallable)
+	void ContinueLoop();
+
+	// Loop result (true=win, false=lose)
+	UPROPERTY(BlueprintAssignable)
+	FOnLoopEvent OnLoopEnd;
+
+protected:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+
+private:
+	int32 TotalIterations; // N
+	int32 AnomalyIteration; // A (0 = no anomaly this run)
+	int32 CurrentIteration; // increments from 1…N
+
+	void FailLoop();
+	void SucceedLoop();
+};
