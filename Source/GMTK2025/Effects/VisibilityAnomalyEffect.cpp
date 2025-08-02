f572a01b987d77cba5c922bf5ce33d969d1b978e
itaruf <79866301+itaruf@users.noreply.github.com>
2025-08-02 16:32:45 +0200

    Add a Visibliity Anomaly Effect that makes the entity invisible when affected

---
+++ Source/GMTK2025/Effects/VisibilityAnomalyEffect.cpp
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "VisibilityAnomalyEffect.h"
+#include "GameFramework/Actor.h"
+
+void UVisibilityAnomalyEffect::Backup_Implementation(AActor* Target)
+{
+	if (Target)
+	{
+		bOriginalHidden = Target->IsHidden();
+	}
+}
+
+void UVisibilityAnomalyEffect::Apply_Implementation(AActor* Target)
+{
+	if (Target)
+	{
+		Target->SetActorHiddenInGame(true);
+		Target->SetActorEnableCollision(false);
+	}
+}
+
+void UVisibilityAnomalyEffect::Revert_Implementation(AActor* Target)
+{
+	if (Target)
+	{
+		Target->SetActorHiddenInGame(bOriginalHidden);
+		if (!bOriginalHidden)
+		{
+			Target->SetActorEnableCollision(true);
+		}
+	}
+}
---
+++ Source/GMTK2025/Effects/VisibilityAnomalyEffect.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AnomalyEffect.h"
+#include "VisibilityAnomalyEffect.generated.h"
+
+UCLASS(Blueprintable)
+class GMTK2025_API UVisibilityAnomalyEffect : public UAnomalyEffect
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Backup_Implementation(AActor* Target) override;
+	virtual void Apply_Implementation(AActor* Target) override;
+	virtual void Revert_Implementation(AActor* Target) override;
+
+private:
+	// What the actors hidden state was before Apply
+	bool bOriginalHidden = false;
+};
